28fb49e40539575010f1a63e4d2876f1212b9bd7
Justin Ellis <justin.ellis18@gmail.com>
2017-12-08 20:27:39 -0500

    pickle

--- deeplearning1/nbs/utils.py
+++ deeplearning1/nbs/utils.py
@@ -1,6 +1,9 @@
 from __future__ import division,print_function
 import math, os, json, sys, re
-import cPickle as pickle
+try:
+    import cPickle as pickle
+except ModuleNotFoundError:
+    import pickle
 from glob import glob
 import numpy as np
 from matplotlib import pyplot as plt
